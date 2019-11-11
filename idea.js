9687c68bc62327bf49322cd5fdc93d96f6e90a29
Eric O'Neill <oneill.eric23@gmail.com>
2019-11-11 16:05:26 -0700

    Finalize styling

---
+++ idea.js
@@ -0,0 +1,40 @@
+class Idea {
+  constructor(obj) {
+    this.id = obj.id;
+    this.title = obj.title;
+    this.body = obj.body;
+    this.star = obj.star || false;
+    this.quality = obj.quality || 0;
+  }
+
+  saveToStorage(ideas) {
+    var allIdeas = JSON.stringify(ideas);
+    localStorage.setItem('ideasArray', allIdeas);
+  }
+
+  deleteFromStorage(index) {
+    ideas.splice(index, 1);
+    this.saveToStorage(ideas);
+  }
+
+  updateIdea(title, body) {
+    this.title = title;
+    this.body = body;
+    this.saveToStorage(ideas);
+  }
+
+  updateStar() {
+    this.star = !this.star;
+    this.saveToStorage(ideas);
+    return this.star;
+  }
+
+  updateQuality(direction) {
+    if (this.quality < (qualities.length - 1) && direction == 'up') {
+      this.quality++;
+    } else if (this.quality > 0 && direction == 'down') {
+      this.quality--;
+    }
+    this.saveToStorage(ideas);
+  }
+}
